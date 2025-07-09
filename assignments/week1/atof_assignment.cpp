69a4de97e5113bdf519112f906a3553014f76ca1
Tarun <tarun.s@intimetec.com>
2025-07-09 17:01:39 +0530

    atof function

---
+++ assignments/week1/atof_assignment.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <iomanip>
+
+
+
+bool isDigit(char ch){
+    return (ch >= 48 && ch <= 57); // checks the ascii range of numerical digits
+}
+
+bool isSpace(char ch){
+    return ch==' ';
+}
+
+//bool flags[] = {0, 0, 0, 0}; //sign, dot, exponent, sign2
+int flag = 0; //0 0 0 0 => expSing, exponent, dot, sign
+
+double custom_atof(const std::string str){
+    double num = 0.0;
+    double afterDecimal = 0.0;
+    int decimalPlaces = 0;
+    int expSign = 1;
+    int expNums = 0;
+
+    double sign = 1.0;
+    for(char ch: str){
+        if(isSpace(ch) && ((flag&15)==0))continue;
+        else if(isspace(ch) && ((flag&15)!=0)){break;}
+
+        if(ch=='+' && ((flag&1)  == 0) && num==0.0){
+           sign = 1.0; 
+           flag = (flag | 1);
+           continue;
+        }else if(ch=='-' && ((flag&1)==0) && num==0.0){
+            sign = -1.0;
+            flag = (flag | 1);
+            continue;
+        }
+
+        if(ch=='.' && ((flag&2)==0)){
+            flag = (flag|2);
+            continue;
+        }else if(ch=='.' && ((flag&2)==1)){
+            break;
+        }
+
+        if(isDigit(ch) && ((flag&2)==0)){
+            num = (num*10) + (ch - '0');
+            continue;
+        }else if(isDigit(ch) && ((flag&2)==2)){
+            decimalPlaces++;
+            afterDecimal = (afterDecimal*10)+(ch-'0');
+        }
+
+        if((ch=='e'||ch=='E') && ((flag&4)==4)){
+            break;
+        }else{
+            flag = flag | 4;
+            continue;
+        }
+
+        if(ch=='+' && ((flag&8)==8)){
+            break;
+        }else{
+            flag = flag | 8;
+            expSign = 1;
+            continue;
+        }
+
+        if(ch=='-' && ((flag&8)==8)){
+            break;
+        }else{
+            flag = flag | 8;
+            expSign = -1;
+            continue;
+        }
+
+        if((flag&4)==4 && isDigit(ch)){
+            expNums = (expNums*10) + (ch-'0');
+            continue;
+        }
+    }
+
+    double result = (num+(afterDecimal/pow(10,decimalPlaces)));
+    result = result*pow(10,(expSign*expNums));
+    return result*sign;
+}
+
+int main(){
+    std::setprecision(9);
+   std::cout<<custom_atof("    1000.312")<<"\n";
+
+    return 0;
+}
