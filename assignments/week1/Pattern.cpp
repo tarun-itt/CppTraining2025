12ef4aea8406a757f011ffa9e9d5e8830935cdbf
Tarun <tarun.s@intimetec.com>
2025-07-11 11:36:38 +0530

    Updated code as per code review

--- assignments/week1/Pattern.cpp
+++ assignments/week1/Pattern.cpp
@@ -1,27 +1,67 @@
 #include <iostream>
 #include <string.h>
 
-bool IsDigit(char ch){
-    return (ch >= 48 && ch <= 57); // checks the ascii range of numerical digits
+bool IsDigit(char currentChar)
+{
+    return (currentChar >= '0' && currentChar <= '9');
 }
 
-bool ValidateInput(const std::string input){
-    for(char c: input){
-        if(!IsDigit(c)) return 0;
+bool ValidateInput(std::string input)
+{
+    for (char currentChar : input)
+    {
+        if (!IsDigit(currentChar))
+            return 0;
     }
     return 1;
 }
 
-int StringToInt(std::string input){
+int StringToInt(std::string input)
+{
     int num = 0;
-    for(char c:input){
-        num = (num*10) + (c-'0');
+    for (char currentChar : input)
+    {
+        num = (num * 10) + (currentChar - '0');
     }
 
     return num;
 }
 
+void printPattern(int len, int width, int spaceStart, int spaceEnd, int patternHeight)
+{
+    std::string toPrint[] = {"1", "0"};
+    int ptr = 0;
 
+    for (int i = 0; i < len; i++, ptr = 0)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (j >= spaceStart && j <= spaceEnd)
+            {
+                std::cout << "  ";
+                ptr = 0;
+            }
+            else if (j < spaceStart || j > spaceEnd)
+            {
+                std::cout << toPrint[ptr] << " ";
+                ptr = (ptr + 1) % 2; 
+            }
+        }
+
+        //increment logic
+        if (i < patternHeight - 1)
+        {
+            spaceStart++;
+            spaceEnd--;
+        }
+        else
+        {
+            spaceStart--;
+            spaceEnd++;
+        }
+        std::cout << "\n";
+    }
+}
 int main()
 {
     std::string input;
@@ -30,53 +70,22 @@ int main()
 
     while (!ValidateInput(input))
     {
-        std::cout<<"Please Enter a valid number (positive integer): ";
-        std::cin>> input;
+        std::cout << "Please Enter a valid number (positive integer): ";
+        std::cin >> input;
     }
     int len = StringToInt(input);
 
-    len = (len*2) + 1;
+    len = (len * 2) + 1;
 
-    int s_start = 1;     // space start
+    int spaceStart = 1; // space start
 
     int height = (len + 1) / 2; // height of the triangular space
 
-    std::string to_print[] = {"1", "0"};
-    int ptr = 0;
-    int width = len%4 == 1 ? len : len+1; // 
+    int width = len % 4 == 1 ? len : len + 1; //
 
-    int s_end = width - 2; // space end
+    int spaceEnd = width - 2; // space end
 
-    for (int i = 0; i < len; i++, ptr = 0)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            if (j >= s_start && j <= s_end)
-            {
-                std::cout << "  ";
-                ptr = 0;
-            }
-            else if (j < s_start || j > s_end)
-            {
-                std::cout << to_print[ptr] << " ";
-                ptr = (ptr + 1) % 2; // reset to print 1 after every iteration
-            }
-        }
-
-        // increment logic for upper triange (space)
-        if (i < height - 1)
-        {
-            s_start++;
-            s_end--;
-        }
-        // for lower triangle  (space)
-        else
-        {
-            s_start--;
-            s_end++;
-        }
-        std::cout << "\n"; //endline for next row
-    }
+    printPattern(len, width, spaceStart, spaceEnd, height);
 
     return 0;
 }
