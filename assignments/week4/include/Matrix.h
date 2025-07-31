050a5653df10e600601b0cd73892a45ca2e7d920
Tarun <tarun.s@intimetec.com>
2025-07-31 17:45:56 +0530

    Refactoring Matrix Calculator

---
+++ assignments/week4/include/Matrix.h
@@ -0,0 +1,42 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <stdexcept>
+#include <string>
+#include <iostream>
+
+class Matrix {
+
+private:
+  int rows;
+  int cols;
+  double **data;
+
+public:
+  Matrix();
+  Matrix(int r, int c);
+  Matrix(const Matrix& other);
+
+  int getRows();
+  int getColumns();
+  void setRows(int rows);
+  void setCols(int cols);
+  double &at(int row, int col);
+  void initialize();
+  bool isValid();
+  void print();
+
+  Matrix operator+(const Matrix &other);
+  Matrix operator*(const Matrix &other);
+  Matrix operator=(const Matrix &other);
+
+  ~Matrix();
+
+  class MatrixError : public std::runtime_error {
+  public:
+    explicit MatrixError(std::string &message)
+        : std::runtime_error("Matrix Error:" + message) {}
+  };
+};
+
+#endif
---
+++ assignments/week4/main.cpp
@@ -0,0 +1,7 @@
+#include "include/MatrixCalculator.h"
+
+int main(){
+    MatrixCalculator::run();
+
+    return 0;
+}
