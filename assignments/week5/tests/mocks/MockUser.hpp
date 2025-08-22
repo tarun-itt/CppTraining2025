5e97f740267e76b0c7a154587ab9316a9e48f277
Tarun <tarun.s@intimetec.com>
2025-08-22 17:32:24 +0530

    Updated cmake and mockuser

--- assignments/week5/tests/mocks/MockUser.hpp
+++ assignments/week5/tests/mocks/MockUser.hpp
@@ -6,9 +6,9 @@
 
 class MockUser : public IUser {
 public:
-    MOCK_CONST_METHOD(std::string, getEmail, (), (const, override));
-    MOCK_CONST_METHOD(std::string, getPassword, (), (const, override));
-    MOCK_CONST_METHOD(uint32_t, getUserId, (), (const, override));
+    MOCK_METHOD(std::string, getEmail, (), (const, override));
+    MOCK_METHOD(std::string, getPassword, (), (const, override));
+    MOCK_METHOD(uint32_t, getUserId, (), (const, override));
     MOCK_METHOD(double, requestBalance, (const LoginResult& loginResult, uint32_t accountNumber), (override));
     MOCK_METHOD(TransactionLedger, requestMiniStatement, (const LoginResult& loginResult, uint32_t accountNumber), (override));
     MOCK_METHOD(TransactionLedger, requestStatementInDateRange, (const LoginResult& loginResult, uint32_t accountNumber, std::string startDate, std::string endDate), (override));
