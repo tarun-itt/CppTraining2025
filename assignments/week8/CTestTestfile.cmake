29d975bc3b6b316ee5079a30ad1cb60dee1d91b6
Tarun <tarun.s@intimetec.com>
2025-08-29 00:14:09 +0530

    Files deleted

--- assignments/week8/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tarunsharma/Documents/CppTraining2025/assignments/week8
-# Build directory: /home/tarunsharma/Documents/CppTraining2025/assignments/week8
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[MusicPlayer_tests]=] "/home/tarunsharma/Documents/CppTraining2025/assignments/week8/MusicPlayer_tests")
-set_tests_properties([=[MusicPlayer_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/tarunsharma/Documents/CppTraining2025/assignments/week8/CMakeLists.txt;71;add_test;/home/tarunsharma/Documents/CppTraining2025/assignments/week8/CMakeLists.txt;0;")
